722a7595114ecd695c088cc4df8ed669d0738d62
CyberNobie <jkarts91@gmail.com>
2025-07-02 12:49:47 +0530

    Time: 2 ms (99.81%), Space: 49.9 MB (40.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,33 @@
 class Solution {
-    private int m, n;
+    public void dfs(char[][] grid,int x,int y)
+    {
+        int m = grid.length;
+        int n= grid[0].length;
 
-    public void dfs(char[][] grid, int x, int y) {
-        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] == '0') return;
+        if(x < 0 || y< 0 || x >=m || y>=n || grid[x][y]=='0')
+        return ;
 
-        grid[x][y] = '0';
+        grid[x][y]= '0';
 
-        dfs(grid, x + 1, y); // Down
-        dfs(grid, x - 1, y); // Up
-        dfs(grid, x, y + 1); // Right
-        dfs(grid, x, y - 1); // Left
+        dfs(grid,x+1,y);
+        dfs(grid,x-1,y);
+        dfs(grid,x,y+1);
+        dfs(grid,x,y-1);
     }
-
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-
-        m = grid.length;
-        n = grid[0].length;
+        if( grid==null || grid.length ==0)
+        return 0;
         int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid, i, j);
-                }
+        
+        int m = grid.length;
+        int n= grid[0].length;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            if(grid[i][j]=='1')
+            {
+                count++;
+                dfs(grid,i,j);
             }
         }
         return count;
