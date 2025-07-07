5615996db5615a9634b91e599cc63f97bafffcfd
CyberNobie <jkarts91@gmail.com>
2025-07-07 12:14:08 +0530

    Time: 29 ms (76.59%), Space: 42.5 MB (88.35%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int x = 2; i >= x * x; x++) {
+                dp[i] = (Math.min(dp[i], 1 + dp[i - x * x]));
+            }
+        }
+        return dp[n];
+    }
+}
