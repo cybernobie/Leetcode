3e5def3ef876568fbf5af08cb6dc222ecfa7bafb
CyberNobie <jkarts91@gmail.com>
2025-07-09 12:56:03 +0530

    Time: 3 ms (99.97%), Space: 41 MB (94.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,31 +1,31 @@
 class Solution {
     int[][] memo;
     int n;
+
     public int numberOfWays(int[] coins, int i, int amount) {
         if (amount == 0) {
             return 1;
-        }      
+        }
         if (i == n || amount < 0) {
             return 0;
-        }     
+        }
         if (memo[i][amount] != -1) {
             return memo[i][amount];
-        }       
-        //This will remove unnecessary call
-        if(coins[i] > amount) {
+        }
+        if (coins[i] > amount) {
             return memo[i][amount] = numberOfWays(coins, i + 1, amount);
         }
-        int take = numberOfWays(coins, i,  amount - coins[i]);
-        int skip = numberOfWays(coins, i + 1, amount);       
-        return memo[i][amount] = take+skip;
+        int take = numberOfWays(coins, i, amount - coins[i]);
+        int skip = numberOfWays(coins, i + 1, amount);
+        return memo[i][amount] = take + skip;
     }
 
     public int change(int amount, int[] coins) {
-        int []dp=new int[amount+1];
-        dp[0]=1;
-        for(int coin:coins){
-            for(int j=coin;j<=amount;j++){
-                dp[j]+=dp[j-coin];
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
             }
         }
         return dp[amount];
