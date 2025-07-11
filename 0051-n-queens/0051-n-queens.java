363bc7169b35688f880937589ff9c5835c685772
CyberNobie <jkarts91@gmail.com>
2025-07-11 11:03:54 +0530

    Time: 5 ms (28.98%), Space: 44.7 MB (95.19%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,78 +1,69 @@
 import java.util.ArrayList;
 
-class Solution
-{
+class Solution {
     List<List<String>> ans = new ArrayList<>();
-    public List<List<String>> solveNQueens(int n){
-        int [][] grid =  new int[n][n];
-        solve(grid,0);
+
+    public List<List<String>> solveNQueens(int n) {
+        int[][] grid = new int[n][n];
+        solve(grid, 0);
         return ans;
     }
 
-    public void addAns(int [][] grid)
-    {
+    public void addAns(int[][] grid) {
         List<String> ls = new ArrayList<>();
-        for(int i=0;i<grid.length;i++)
-        {
-            String curr="";
-            for(int j=0;j<grid.length;j++)
-            {
-                if(grid[i][j]==0)
-                {
-                    curr+=".";
-                }
-                else{
-                    curr+="Q";
+        for (int i = 0; i < grid.length; i++) {
+            String curr = "";
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] == 0) {
+                    curr += ".";
+                } else {
+                    curr += "Q";
                 }
             }
             ls.add(curr);
         }
         ans.add(ls);
     }
-    public void solve(int[][] grid, int row)
-    {
-        if(grid.length==row)
-        {
+
+    public void solve(int[][] grid, int row) {
+        if (grid.length == row) {
             addAns(grid);
             return;
         }
-        for(int j=0;j<grid.length;j++)
-        {
-            if(isPossible(row,j,grid))
-            {
-                grid[row][j]=1;
-                solve(grid,row+1);
-                grid[row][j]=0;
+        for (int j = 0; j < grid.length; j++) {
+            if (isPossible(row, j, grid)) {
+                grid[row][j] = 1;
+                solve(grid, row + 1);
+                grid[row][j] = 0;
             }
         }
     }
-    public boolean isPossible(int row,int col,int[][] grid)
-    {
+
+    public boolean isPossible(int row, int col, int[][] grid) {
         int x = row;
         int y = col;
 
-        while(x>=0)
-        {
-            if(grid[x--][y]==1) return false;
+        while (x >= 0) {
+            if (grid[x--][y] == 1)
+                return false;
         }
 
         // left diagonal
 
         x = row;
         y = col;
-        while(x>=0 && y>=0)
-        {
-            if(grid[x--][y--]==1)
-            return false;
+        while (x >= 0 && y >= 0) {
+            if (grid[x--][y--] == 1)
+                return false;
         }
 
         // right diagonal
         x = row;
         y = col;
-        while(x>=0 && y<grid.length)
-        {
-            if(grid[x--][y++]==1) return false;
-        } 
+        while (x >= 0 && y < grid.length) {
+            if (grid[x--][y++] == 1)
+                return false;
+        }
         return true;
 
     }
