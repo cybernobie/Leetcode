22cf5f8ef85309c3d11d734ff1071b48a865d820
CyberNobie <jkarts91@gmail.com>
2025-07-11 11:35:07 +0530

    Time: 381 ms (10.01%), Space: 40.9 MB (85.71%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -6,21 +6,22 @@ public class Solution {
     private boolean solve(char[][] board) {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-    
-                if (board[row][col] != '.') continue;
 
+                if (board[row][col] != '.')
+                    continue;
 
                 for (char num = '1'; num <= '9'; num++) {
                     if (isValid(board, row, col, num)) {
                         board[row][col] = num;
 
-                        if (solve(board)) return true; 
-                        
+                        if (solve(board))
+                            return true;
+
                         board[row][col] = '.';
                     }
                 }
 
-                  return false;
+                return false;
             }
         }
 
@@ -29,9 +30,12 @@ public class Solution {
 
     private boolean isValid(char[][] board, int row, int col, char num) {
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) return false;
-            if (board[i][col] == num) return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) return false; 
+            if (board[row][i] == num)
+                return false;
+            if (board[i][col] == num)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num)
+                return false;
         }
         return true;
     }
